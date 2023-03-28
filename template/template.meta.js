08e4bc7d2c6f43ef40324f2b01dc27d0a7bd136a
jameshsu1125 <jameshsu1125@gmail.com>
2023-03-28 14:39:20 +0800

    add html mata data

--- template/template.meta.js
+++ template/template.meta.js
@@ -1,5 +1,6 @@
 module.exports = {
-	title: 'toyota-crown',
-	description: '',
+	title: 'CROWN JOURNEY',
+	description:
+		'CROWN 榮耀登場，以日本極致造車工藝，精益求精的專注每處細節、每道工法，打造全新斜背跨界跑旅，邀您一起鑑賞',
 	url: 'https://www.toyota.com.tw/event/202303_CROWN/',
 };
