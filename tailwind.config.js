0cde79ed5c6cd4e3f953a6dfa7e01bef09a07173
jameshsu1125 <jameshsu1125@gmail.com>
2023-03-29 16:33:08 +0800

    add LiSong Pro for serif font

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
 		},
 		fontFamily: {
 			notoSans: ['Noto Sans TC', 'sans-serif'],
-			notoSerif: ['Noto Serif TC', 'PMingLiU', '新細明體', 'serif'],
+			notoSerif: ['Noto Serif TC', 'PMingLiU', '新細明體', 'LiSong Pro', 'serif'],
 			sourceSansPro: ['Source Sans Pro', 'sans-serif'],
 		},
 	},
